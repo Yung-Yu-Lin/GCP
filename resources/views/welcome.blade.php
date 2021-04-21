a1cc7b76ba4c19f4d9484407d47792bff41d68d5
Gary <wfjh10602@gmail.com>
2021-04-21 21:54:25 +0800

    rebranding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Update
+                    Gary
                 </div>
 
                 <div class="links">
