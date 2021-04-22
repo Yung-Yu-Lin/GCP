4b32418bf0db4d621a9641c0cb18d65e7a1e8bd6
Gary <wfjh10602@gmail.com>
2021-04-22 13:50:54 +0800

    rebranding3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Gary love Bebe
+                    1
                 </div>
 
                 <div class="links">
